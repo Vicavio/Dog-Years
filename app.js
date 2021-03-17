3d57642086324e806aea7b97274103cb676039ee
Vicavio <cuenta.vica@gmail.com>
2021-03-17 20:02:17 +0100

    Commit both files

---
+++ app.js
@@ -0,0 +1,25 @@
+//Adding an number to myAge var
+let myAge = 26;
+
+//Adding a value to this var
+let earlyYears = 2;
+
+earlyYears = 2 * 10.5;
+
+//I substract 2 from myAge var
+let laterYears = myAge - 2;
+
+//I multiplied by 4 laterYears var
+laterYears = laterYears * 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+//Summing earlyYears and laterYears to discover my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+//Adding a string to myName in lower case
+let myName = 'Viorica'.toLowerCase();
+//Printing this sentence
+console.log(`My is name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.)
+`)
